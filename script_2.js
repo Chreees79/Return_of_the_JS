4d747fa0fdd9a94155381774b711e46272653d35
xko <germain.rgb@hotmail.fr>
2022-09-27 11:55:46 +0200

    script 2 OK

--- script_2.js
+++ script_2.js
@@ -18,55 +18,42 @@ const books = [
 function titlesOfBooks(array) {
   return array.map(x => x.title)
 };
-console.log(titlesOfBooks(books));
-//Same but other way
-const titlesOf = array => array.map(x => x.title);
-console.log(titlesOf(books));
+console.log(titlesOfBooks(books))
 
 //Have all the book been rented at least once ?
 
 console.log('All books rented at least once ?')
-const rentedOnce = array => array.map(x => x.rented > 0 ? 'Yes' : 'No')
-console.log(rentedOnce(books));
 
-console.log('All books rented at least once ?')
-function rentedAtLeastOnce(array) {
-  if (array.every( x => x.rented > 0 ))
-    return 'Yes'
-  else 
-    return 'No'
+function booksRentedAtLeastOnce(array) {
+  return array.every(book => book.rented > 0) ? 'Yes' : 'No'  
 };
-console.log(rentedAtLeastOnce(books));
-
-//Withour IF parameters
-console.log('TEST')
-const rentedAtOnce = books.every(x => x.rented > 0 ) ? 'Yes' : 'No';
-const rentedLeastOnce = array => array.every(x => x.rented > 0 ) ? 'Yes' : 'No';
-console.log(rentedAtOnce)
-console.log(rentedLeastOnce(books))
+console.log(booksRentedAtLeastOnce(books))
 
 //most rented book
 
 console.log('Most rented book')
+
 function mostRented(array) {
-  return array.reduce((a,b) => a.rented > b.rented? a : b);
-}
-console.log(mostRented(books));
+  return array.reduce((book1, book2) => book1.rented > book2.rented? book1 : book2);
+};
+console.log(mostRented(books))
 
 //Least rented one
 
 console.log('Least rented one')
+
 function leastRented(array) {
-  return array.reduce((a, b) => a.rented < b.rented? a : b);
-}
-console.log(leastRented(books));
+  return array.reduce((book1, book2) => book1.rented < book2.rented? book1 : book2);
+};
+console.log(leastRented(books))
 
 //Delete books with ID 133712
 
 console.log('Delete book with ID 133712 from Array')
+
 function delelteId(array) {
-  let book = array.findIndex((x) => x.id === 133712);
-  return array.splice(book, 1);
-}
-console.log(delelteId(books));
-console.log(books);
+  let book = array.findIndex((x) => x.id === 133712)
+  return array.splice(book, 1)
+};
+console.log(delelteId(books))
+console.log(books)
